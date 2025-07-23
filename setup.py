14afcd84a0ec6f468243a736faf703c43fb10e22
victorique-de-blois <haoyuan@cs.ucla.edu>
2025-07-23 15:24:55 -0700

    delete files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 assert sys.version_info.major == 3 and sys.version_info.minor >= 6, "python version >= 3.6 is required"
 
 setup(
-    name='pvp',
+    name='aim',
     version='0.0.1',
     packages=find_packages(),
 )
